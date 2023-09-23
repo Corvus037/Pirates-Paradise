68acc8d84953ee210dff57ade3486e17b6bedaf6
Corvus037 <nathanof2003@outlook.com>
2023-09-23 14:21:26 -0300

    Jogo Feito finalmente

--- Pirate's Paradise/Assets/Scripts/EndSceneManager.cs
+++ Pirate's Paradise/Assets/Scripts/EndSceneManager.cs
@@ -6,6 +6,8 @@ public class EndSceneManager : MonoBehaviour
 {
     public Text scoreText;
 
+   
+
     private void Start()
     {
         ScoreSystem scoreSystem = FindObjectOfType<ScoreSystem>();
@@ -20,6 +22,21 @@ public class EndSceneManager : MonoBehaviour
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("Game");
+        // Verifica se o ScoreSystem já existe na cena atual.
+        ScoreSystem scoreSystem = FindObjectOfType<ScoreSystem>();
+        
+        if (scoreSystem != null)
+        {
+            // Atualiza o valor dos pontos no objeto existente.
+            scoreSystem.AddScore(0); // Valor zero apenas para atualizar a exibição.
+
+            // Carrega a cena "Game".
+            SceneManager.LoadScene("Game");
+        }
+        else
+        {
+            // Caso não encontre o ScoreSystem na cena, apenas carrega a cena "Game".
+            SceneManager.LoadScene("Game");
+        }
     }
 }
